36a48db1ed79a91e71dd5e539f51879498552669
PPfeedback <39222744+PPfeedback@users.noreply.github.com>
2018-09-29 21:54:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,6 +27,15 @@ async def on_message(message):
             await client.send_message(server.get_channel("491676268853723138"), "@everyone %s" % (" ".join(args[1:])))
         else:
             await client.send_message(message.channel, "You are not an admin")
+    if message.content.upper().startswith('?WARNING'):
+        if message.author.id == "268087923751124992":
+            server = client.get_server("490938256683302912")
+            args = message.content.split(" ")
+            embed=discord.Embed(title=":warning: Warning from Admin", description="Warning from an adminstator.", color=0x00ff00)
+            embed.add_field(name="Warning Reason", value="%s @here" % (args[1]), inline=False)
+            await client.send_message(server.get_channel("490942613269250048"), embed=embed)
+        else:
+            await client.send_message(message.channel, "You do not meet the requirements to complete this action.") 
 
 @client.event
 async def on_ready():
