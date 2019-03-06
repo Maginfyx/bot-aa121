ecc10ae0c1faf9d5463200484797c9c3deddf8b1
Execed <39222744+PPfeedback@users.noreply.github.com>
2019-03-06 12:51:37 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,8 +19,7 @@ async def on_message(message):
         await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
     if message.content.upper().startswith('PP!HELP'):
         userID = message.author.id
-        await client.send_message(message.channel, "https://cdn.discordapp.com/attachments/547896017828184067/552832679280902145/unknown.png" % (userID))
-
+        await client.send_message(message.channel, "<@%s> https://cdn.discordapp.com/attachments/547896017828184067/552832679280902145/unknown.png" % (userID))
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name="Cooking Pizzas!"))
