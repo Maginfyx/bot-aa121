067b3ee4c0928f2af64c928336949f23270f8c71
Execed <39222744+PPfeedback@users.noreply.github.com>
2019-03-06 12:42:06 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,9 +25,7 @@ async def on_message(message):
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name="Cooking Pizzas!"))
-    
-@client.event
-async def on_ready():
+   
       
   
     
