60311d9d6fa7c4bcb745e515b95266d8c50251c5
Execed <39222744+PPfeedback@users.noreply.github.com>
2019-03-07 17:08:50 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,16 +24,22 @@ async def on_message(message):
     if message.content.upper().startswith('PP!SUBJACK'):
         userID = message.author.id
         await client.send_message(message.channel, "Subscribe to Jack here: https://bit.ly/2H4woiP")
+    if message.content.upper().startswith('PP!MEGAOOFSPAM'):
+        await bot.say(random.choice(["@everyone DIE",
+                                             "@everyone Do you agree Jack has a penis so small you can't see it?",
+                                             "@everyone Also, you have a bitch face so go fuck yourself!",
+                                             "@everyone PAY 500 US DOLLARS OR NO COMPUTER AND CONSOLE FOR A YEAR",
+                                             "@everyone Ok please get out the fucking room I'm playing minecraft!",
+                                             "@everyone Mommy...PLEASE FUCKING LET ME MAKE THIS MINECRAFT MINEPLEX BEDWARS VIDEO!!1!11!",
+                                             "@everyone WHO WANTS ADMIN? SIMPLY GO TO PORNHUB.COM AND REDEEM YOUR PRIZE BY WATCHING 1,000 PORN VIDEOS! NO LOGIN INFO OR DOWNLOADS!",
+                                             "@everyone Nivea Soft Cream...More Like Nivea Soft Dick",
+                                             "@everyone Jack is a cunt lmao"]))  
 
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name="Cooking Pizzas!"))
     
    
-@client.event
-async def on_ready():
-    await client.send_message(server.get_channel("514861708230131722"), "Bot has been updated and restarted. If you find any bugs in this update, please message Magi#2011!")
-  
     
 client.run(str(os.environ.get('bottoken')))
  
